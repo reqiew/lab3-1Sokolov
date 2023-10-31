df770736c7b211562c4f3e9ebd59da87342c38b2
Danila <144239949+reqiew@users.noreply.github.com>
2023-10-31 04:20:20 +0300

    Add files via upload

---
+++ lab3-1Sokolov/lab3-1Sokolov/Program.cs
@@ -0,0 +1,21 @@
+﻿//средний уровень 18
+try
+{
+    Console.Write("Введите k:");
+    int k = int.Parse(Console.ReadLine());
+    double Z = 1;
+    double S = 0;
+    for (int j = -1; j <= k; j++)
+    {
+        if (j != -14) Z *= (j - j * j) / (j + 14);
+        for (int i = j; i <= k+3; i++)
+        {
+            if (i != k + 2) S += (Math.Pow(Math.Abs(i - 5), 1 / 4)) / (Math.Abs(i-7));
+        }
+    }
+    Console.WriteLine($"Z={Z*S:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
